9779f14b9f3c3e49f4b5901ae10eabe982cd96ac
UncleRabbits <mengdibaev@gmail.com>
2021-01-23 00:17:15 +0400

    index page

--- components/menu.js
+++ components/menu.js
@@ -8,7 +8,7 @@ export function Menu() {
       <label htmlFor="isMenuOpen" className="w-full h-full bg-black bg-opacity-50 absolute left-0 top-0 cursor-pointer z-1" />
       <div className="w-96 bg-maingray h-full shadow relative z-2">
         <div className="flex items-center h-20 w-full px-5">
-          <div className="bg-mainbg px-5 h-12 text-white font-h1 rounded-xl shadow-inner flex-1 mr-2">OceanBlades</div>
+          <div className="bg-mainbg px-5 h-12 text-white font-h1 rounded-xl shadow-inner flex-1 mr-2 text-center">OceanBlades</div>
           <label htmlFor="isMenuOpen" className="px-2 h-12 bg-mainbg rounded-xl shadow-inner flex items-center cursor-pointer">
             <img src="/img/menu.svg" alt="" />
           </label>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import Header from "../components/header";
+import Wrapper from "../components/wrapper";
+import { discordLink, rules } from "../config/indexPage";
 
 export default function Home() {
   return (
     <div className="bg-mainbg w-full min-h-screen">
       <Header />
+      <Wrapper>
+        <div class="font-h1 text-white mb-5">Условия вступления:</div>
+        <div class="text-white font-text whitespace-pre-line text-left mb-10">{rules}</div>
+        <div class="font-h1 text-white mb-5">Полезные ссылки:</div>
+        <div class="font-h2 text-white mb-2">Дискорд: <a href={discordLink} target="_blank" className="text-blue-600 cursor-pointer">клик</a></div>
+      </Wrapper>
     </div>
   )
 }
